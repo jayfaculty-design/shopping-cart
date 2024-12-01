f9d942ecb88aec6054d8e109cae4c007bda3008e
Jay Designs <izfaculty12@gmail.com>
2024-12-01 12:14:03 +0000

    Done

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,5 +1,3 @@
-import { faLeanpub } from "@fortawesome/free-brands-svg-icons";
-import { useScrollTrigger } from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
