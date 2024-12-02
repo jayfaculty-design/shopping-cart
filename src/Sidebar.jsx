e23f4e481ff99726b9038110770d868e9c31494b
Jay Designs <izfaculty12@gmail.com>
2024-12-02 15:07:58 +0000

    mad

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -10,7 +10,7 @@ function Sidebar() {
       <div
         className={`${
           isOpen ? "right-0" : "-right-full"
-        } transition-all duration-300 ease-in-out side-bar z-[999] bg-white h-[100vh] fixed w-[50%] right-0 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]`}
+        } transition-all duration-300 ease-in-out side-bar z-[999] bg-white h-[100vh] fixed w-[50%]  shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]`}
       >
         <div className="flex justify-between items-center pl-5 pr-5 pb-5 pt-5 ">
           <h1 className="font-semibold">Items(0)</h1>
